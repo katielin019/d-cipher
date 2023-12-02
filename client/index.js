3dab12f35ab186fbb88cb6283852d57c11612af7
Katie Lin <katielin019@gmail.com>
2023-12-02 16:44:08 -0500

    jQuery not working

--- client/index.js
+++ client/index.js
@@ -1,11 +1,27 @@
 $(document).ready(() => {
+    alert("hi");
     var puzzle = "";
     var key = {};
 
-    $.getJSON("sample.json", function(data) {
-        puzzle = data.puzzle_string;
-        key = data.puzzle_key[0];
-    }).fail(function() {
-        alert("Error occured while loading JSON")
-    })
+    let currentColor = 'white'; // have to define as a semi-global var
+    $('#btnDemo').click( () => {
+        if (currentColor === 'white') {
+            currentColor = 'blue';
+        } else {
+            currentColor = 'white';
+        }
+
+        $('#header').css('background-color', currentColor);
+        $('#header').text('Class Demo - ' + currentColor);
+    });
+
+    // $.getJSON("sample.json", function(data) {
+    //     puzzle = data.puzzle_string;
+    //     key = data.puzzle_key[0];
+    //     alert("puzzle loaded: " + puzzle)
+    // }).fail(function() {
+    //     alert("Error occured while loading the sample puzzle. Try refreshing the page.")
+    // })
+
+    // $("#solver-container").append(puzzle);
 });
