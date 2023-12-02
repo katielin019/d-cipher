1ee1273166f5a3a059569b925419bd259719bb09
Katie Lin <katielin019@gmail.com>
2023-12-02 16:52:20 -0500

    Fixed jQuery load problem. Can display sample cipher (retrieved from json file)

--- client/index.js
+++ client/index.js
@@ -1,27 +1,12 @@
 $(document).ready(() => {
-    alert("hi");
     var puzzle = "";
     var key = {};
 
-    let currentColor = 'white'; // have to define as a semi-global var
-    $('#btnDemo').click( () => {
-        if (currentColor === 'white') {
-            currentColor = 'blue';
-        } else {
-            currentColor = 'white';
-        }
-
-        $('#header').css('background-color', currentColor);
-        $('#header').text('Class Demo - ' + currentColor);
-    });
-
-    // $.getJSON("sample.json", function(data) {
-    //     puzzle = data.puzzle_string;
-    //     key = data.puzzle_key[0];
-    //     alert("puzzle loaded: " + puzzle)
-    // }).fail(function() {
-    //     alert("Error occured while loading the sample puzzle. Try refreshing the page.")
-    // })
-
-    // $("#solver-container").append(puzzle);
+    $.getJSON("sample.json", function(data) {
+        puzzle = data.puzzle_string;
+        key = data.puzzle_key[0];
+        $("#solver-container").text(puzzle);
+    }).fail(function() {
+        alert("Error occured while loading the sample puzzle. Try refreshing the page.")
+    })
 });
