3b825c1fbf493e1a35d0bca58697c39e0a7bc266
Katie Lin <katielin019@gmail.com>
2023-12-02 16:35:35 -0500

    Modified sample puzzle

--- client/index.js
+++ client/index.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-    
+    var puzzle = "";
+    var key = {};
+
+    $.getJSON("sample.json", function(data) {
+        puzzle = data.puzzle_string;
+        key = data.puzzle_key[0];
+    }).fail(function() {
+        alert("Error occured while loading JSON")
+    })
 });
