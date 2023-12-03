54bb8307906838fa4c2c4e9417037a424297e2f2
Katie Lin <katielin019@gmail.com>
2023-12-03 15:57:36 -0500

    Reads puzzle input on button press

--- client/index.js
+++ client/index.js
@@ -2,11 +2,18 @@ $(document).ready(() => {
     var puzzle = "";
     var key = {};
 
+    // Load default puzzle
     $.getJSON("sample.json", function(data) {
         puzzle = data.puzzle_string;
         key = data.puzzle_key[0];
         $("#cipher-display").text(puzzle);
     }).fail(function() {
-        alert("Error occured while loading the sample puzzle. Try refreshing the page.")
-    })
+        alert("Error occured while loading the sample puzzle. Try refreshing the page.");
+    });
+
+    // Get puzzle input
+    $("#read-puzzle").click( () => {
+        puzzle = $("#puzzle-input").val().toUpperCase();
+        $("#cipher-display").text(puzzle);
+    });
 });
