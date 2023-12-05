5ddbb3b042f1a6d8b925346c676fe7fc6b173bb6
Katie Lin <katielin019@gmail.com>
2023-12-05 16:52:21 -0500

    Added jQuery code to generate HTML objects dynamically; some method not being called - need to debug

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
     var puzzle;
     var ALPHA = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    
     // Load default puzzle
     $.getJSON("sample.json", function(data) {
         puzzle = data.puzzle_string;
@@ -10,53 +11,60 @@ $(document).ready(() => {
         alert("Error occured while loading the sample puzzle. Try refreshing the page.");
     });
 
-    // Get puzzle input
-    $("#read-puzzle").click( () => {
-        puzzle_string = $("#puzzle-input").val().toUpperCase();
-    });
+    // // Get puzzle input
+    // $("#read-puzzle").click( () => {
+    //     puzzle_string = $("#puzzle-input").val().toUpperCase();
+    // });
 
     function renderPuzzle(puzzle_string) {
         $("#cipher-display").text(puzzle_string);
         $("#decoder-display").text(puzzle_string);
-        let uniques = activateSolver(puzzle_string);
-        $("#solver-txt").text(uniques);
+        var letterSet = activateSolver(puzzle_string);
+        alert("hi");
+        // $("#solver-txt").text(uniques);
 
-        // hard coding this for now
-        for (i = 0; i < 18; i++) {
-
-        }
+        var letterArr = Array.from(letterSet);
+        letterArr.forEach(function(letter) {
+            let box = getLetterboxHTML(letter);
+            $('.input-row').append(box);
+            let label = getLabelHTML(letter);
+            $('.label-row').append(label);
+        });
 
         freqs = calculateFrequencies(puzzle_string);
         $("#frequencies").text(JSON.stringify(freqs));
         $("#tips").text("The most frequent letters in the English language are ETAOIN");
+    }
 
-        // var words = puzzle_string.split();
-        
-        // words.forEach(function(word) {
-        //     for (var i = 0; i < word.length; i++) {
-        //         var currLetter = word.charAt(i);
-        //     }
-        // })
+    function getLabelHTML(letter) {
+        // <td class="letter">Q</td>
+        var htmlOpen = "<td class='letter'>";
+        var htmlClose = "</td>";
+        return htmlOpen + letter + htmlClose;
     }
 
     function getLetterboxHTML(letter) {
-        // <td><textarea class="solver-inputs" id="Q" cols="1" rows="1" maxlength="1"></textarea></td>
-        var html = "<";
+        var htmlOpen = "<td><textarea class='letterbox' id='";
+        var htmlClose = "' cols='1' rows='1' maxlength='1'></textarea></td>";
+        return htmlOpen + letter + htmlClose;
     }
 
     function activateSolver(puzzle_string) {
-        var c, letters, result;
-        result = "";
+        var c, letters;
+        // var c, letters, result;
+        // result = "";
         letters = new Set();
 
         for (i = 0; i < puzzle_string.length; i++) {
             c = puzzle_string.charAt(i);
-            if (ALPHA.includes(c) && !letters.has(c)) {
+            if (ALPHA.includes(c)) {
                 letters.add(c);
             }
         }
-        result = [...letters].join(' ');
-        return result;
+        // result = [...letters].join(' ');
+        // return result;
+        alert(letters.size());
+        return letters;
     }
 
     // adapted from https://github.com/fidian/rumkin-cipher
