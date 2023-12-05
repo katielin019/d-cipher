33d36041fa080320b8d15aec19ef256214ac254e
Katie Lin <katielin019@gmail.com>
2023-12-05 12:14:07 -0500

    Working on instantiating solver

--- client/index.js
+++ client/index.js
@@ -18,6 +18,8 @@ $(document).ready(() => {
     function renderPuzzle(puzzle_string) {
         $("#cipher-display").text(puzzle_string);
         $("#decoder-display").text(puzzle_string);
+        let uniques = activateSolver(puzzle_string);
+        $("#solver").text(uniques);
         freqs = calculateFrequencies(puzzle_string);
         $("#frequencies").text(JSON.stringify(freqs));
         $("#tips").text("The most frequent letters in the English language are ETAOIN");
@@ -29,6 +31,19 @@ $(document).ready(() => {
         //         var currLetter = word.charAt(i);
         //     }
         // })
+    }
+
+    function activateSolver(puzzle_string) {
+        var j, char, unique;
+        unique = [];
+
+        for (j = 0; j < puzzle_string.length; j++) {
+            char = puzzle_string.charAt(j)
+            if (ALPHA.includes(char) && !unique.includes(char)) {
+                unique.push(char);
+            }
+        }
+        return unique.toString();
     }
 
     // adapted from https://github.com/fidian/rumkin-cipher
