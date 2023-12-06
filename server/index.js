5f7ae9d6f01ff2af6c940d86221b5bdc46a7e6f4
Katie Lin <katielin019@gmail.com>
2023-12-06 08:52:45 -0500

    Can process JSON files for all ngrams

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
+'use strict';
+
 const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
 const app = express();
 const port = 3002;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(cors({
+	origin: 'http://localhost:3000'
+}));
+
+var charFreqs = [];
+var bigrams = [];
+var trigrams = [];
+
+// URL: /letterFreq?letter=A
+app.get('/letterFreq', (req, res) => {
+	const letter = req.query.letter;
+	const freq = 
 	res.send('GET: Hello world!');
 });
 
@@ -19,5 +36,19 @@ app.delete('/', (req, res) => {
 });
 
 app.listen(port, () => {
+	let rawData;
+	rawData = fs.readFileSync('./data/english_1grams.json');
+	charFreqs = JSON.parse(rawData);
+	console.log('charFreqs populated; count = ', charFreqs.length);
+
+	rawData = fs.readFileSync('./data/english_2grams.json');
+	bigrams = JSON.parse(rawData);
+	console.log('bigrams populated; count = ', bigrams.length);
+
+	rawData = fs.readFileSync('./data/english_3grams.json');
+	trigrams = JSON.parse(rawData);
+	console.log('trigrams populated; count = ', trigrams.length);
+
+
 	console.log('Listening on port ', port);
 });
